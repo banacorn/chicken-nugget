08f57b9bde7ddd9d6b2406b74f642011f545a043
blitz <banacorn@gmail.com>
2012-12-05 12:57:57 +0800

    bytestring rules

--- compressor.hs
+++ compressor.hs
@@ -2,14 +2,15 @@
 
 import Foreign.C
 import Foreign hiding (unsafePerformIO, void)
-import Data.Char (chr, ord)
-import Data.Ord (comparing)
-import Data.List (sortBy)
+--import Data.Char (chr, ord)
+--import Data.Ord (comparing)
+--import Data.List (sortBy)
+import qualified Data.ByteString as B
 import System.IO
 import System.IO.Unsafe (unsafePerformIO)
-import System.Environment (getArgs)     
-import Control.Monad
-import Control.Concurrent
+--import System.Environment (getArgs)     
+--import Control.Monad
+--import Control.Concurrent
 
 foreign import ccall "lzfx.h lzfx_compress"
     c_compress :: Ptr CUChar -> CUInt -> Ptr CUChar -> Ptr CUInt -> IO CInt
@@ -17,80 +18,19 @@ foreign import ccall "lzfx.h lzfx_compress"
 foreign import ccall "lzfx.h lzfx_decompress"
     c_decompress :: Ptr CUChar -> CUInt -> Ptr CUChar -> Ptr CUInt -> IO CInt
 
-adapter :: (Ptr CUChar -> CUInt -> Ptr CUChar -> Ptr CUInt -> IO CInt) -> Int -> String -> String
-adapter function chunkSize content = map toChar $ unsafePerformIO $ do
-    -- allocate buffers
-    inputBuffer <- mallocArray chunkSize
-    outputBuffer <- mallocArray chunkSize
-    outputSize <- mallocBytes 32
-    -- put it in
-    pokeArray inputBuffer (map fromChar content)
-    poke outputSize (fromIntegral chunkSize)
-    -- compress
-    function inputBuffer (fromIntegral chunkSize) outputBuffer outputSize
-    -- take it out
-    output <- peekArray chunkSize outputBuffer
-    outputLength <- peek outputSize
-    putStrLn $ show outputLength
-    -- free buffers
-    free inputBuffer
-    free outputBuffer
-    free outputSize
-    return $ take (fromIntegral outputLength) output
-    where   toChar = chr . fromIntegral
-            fromChar = fromIntegral . ord
-
-compress :: Int -> String -> String
-compress = adapter c_compress
-
-decompress :: Int -> String -> String
-decompress = adapter c_decompress
-
-readInputFile :: FilePath -> IO String
-readInputFile filename = openBinaryFile filename ReadMode >>= hGetContents
+compress :: Int -> B.ByteString -> B.ByteString
+compress chunkSize input = unsafePerformIO $ do
+    return $ B.pack [1..4]
 
 
-chunk :: Int -> [a] -> [[a]]
-chunk size [] = []
-chunk size thunk = a : chunk size rest
-    where (a, rest) = splitAt size thunk
+reader filename = do
+    B.readFile filename >>= putStrLn . show
 
-main =
-    let 
-        d = "banacorn's favorite tracks"
-        d' = compress 64 d
-        d'' = decompress 64 d'
-    in do
-    putStrLn $ show (length d) ++ " " ++ show (map ord d)
-    putStrLn $ show (length d') ++ " " ++ show (map ord d')
-    putStrLn $ show (length d'') ++ " " ++ show (map ord d'')
-    --(chunkSize, threadNumber, inputFilename, outputFilename) <- getArgs >>= processArgs
-    --chunks <- readInputFile inputFilename >>= return . zip [0..] . chunk chunkSize
-    --exit <- newEmptyMVar
-    --input <- newEmptyMVar
-    --output <- newEmptyMVar
-    --dispatcher input chunks
-    --compressor threadNumber chunkSize input output
-    --collector (length chunks) outputFilename output exit
-    --takeMVar exit
-    --putStrLn "hello"
-
-processArgs args = 
-    let [chunkSize, threadNumber, input, output] = args in
-    if length args /= 4 then do
-        putStrLn "mcompress <chunkSize> <threadNumber> <input> <output>"
-        return (0, 0, "", "")
-    else
-        return (read chunkSize, read threadNumber, input, output)
+chunk :: Int -> B.ByteString -> [B.ByteString]
+chunk size string = if B.null string then [] else B.take size string : chunk size (B.drop size string)
 
 
-compressor threadNumber chunkSize input output = replicateM_ threadNumber . forkIO . forever $ takeMVar input >>= putMVar output . processData
-    where processData (a, b) = (a, compress chunkSize b)
+a = B.pack [1..10]
 
-dispatcher input chunks = forkIO $ mapM_ (putMVar input) chunks
+main = putStrLn $ show $ chunk 3 a
 
---collector :: Int -> MVar Int -> MVar Bool -> IO ThreadId
-collector chunkNumber outputFilename output exit = forkIO $ do
-    replicateM chunkNumber (takeMVar output)
-    >>= putStrLn . show . length . concat . snd . unzip . sortBy (comparing fst)
-    >> putMVar exit True
